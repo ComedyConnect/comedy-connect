5eafb95681f8a0ba9c85f69ae36e28f652039e93
ComedyConnect <141169350+ComedyConnect@users.noreply.github.com>
2023-09-02 12:38:01 +0000

    React js basic setup

---
+++ src/routers/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/login/Login";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/login" Component={Login} />
+    </Routes>
+  );
+};
+
+export default Routers;
